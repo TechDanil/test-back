9053a0fa0c34eea54ab1103e9f65e1522f6670e9
Daniel <komarov@arnca.pro>
2024-10-14 16:59:35 +0500

    vercel

--- src/shared/configs/db.ts
+++ src/shared/configs/db.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2'
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,7 +7,7 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
-    dialect: 'mysql',
+    dialectModule: mysql2,
 });
 
 export default sequelize
